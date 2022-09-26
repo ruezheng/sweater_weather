6a879b59f72b95b8c5eda300b9bd6030fa45c3c0
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-25 21:45:01 -0600

    feat: create ForecastController index action

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::ForecastController < ApplicationController
+
+  def index
+    if params[:location]
+      location = MapquestFacade.create_coordinates(params[:location])
+      forecast = OpenweatherFacade.create_forecast(location)
+
+      render json: ForecastSerializer.new(forecast)
+    else
+      render json: { error: 'Location Required' }, status: 404
+    end
+  end
+end
+ 
