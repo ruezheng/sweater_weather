db043e949a6b61600caaaf54323de41c73f68b4f
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-26 10:50:51 -0600

    test: add number of books found from search results

--- spec/poros/book_spec.rb
+++ spec/poros/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Book do
-  it "returns book poro from JSON response with title, isbn, and publisher as attributes" do
+  it "returns book poro from JSON response with title, isbn, publisher, and total number of books found as attributes" do
     response = JSON.parse(File.read('spec/fixtures/openlibrary_response.json'), symbolize_names: true)
     book_data =  response[:docs][0]
 
@@ -11,5 +11,6 @@ describe Book do
     expect(book.title).to eq('Denver, Co')
     expect(book.isbn).to eq ["0762507845", "9780762507849"]
     expect(book.publisher).to eq ["Universal Map Enterprises"]
+    expect(book.num_found).to eq(47113)
   end
 end
