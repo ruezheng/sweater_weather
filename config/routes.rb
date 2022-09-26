3d5c1bfff12d2263d1fc40e671cbcad3fe4b6478
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-25 22:09:35 -0600

    feat: create route 'POST /api/v1/users #create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
+
+      resources :users, only: [:create]
     end
   end
 end
