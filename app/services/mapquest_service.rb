ee3b687e783bbc9077ebf1ef695828ab5dd173ad
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-25 21:06:56 -0600

    feat: add params to retrieve location data ::get_coordinates

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -3,5 +3,10 @@ class MapquestService
     Faraday.new(url: "http://www.mapquestapi.com/")
   end
 
-
+  def self.get_coordinates(location)
+    response = conn.get("geocoding/v1/address") do |req|
+      req.params['key'] = ENV['mapquest_api_key']
+      req.params['location'] = location
+    end
+  end
 end
