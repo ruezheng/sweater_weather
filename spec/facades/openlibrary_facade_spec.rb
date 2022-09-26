09f3c2c64bd61be8c1a4fcbaca189a7fa262d852
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-26 10:22:58 -0600

    test: OpenlibraryFacade.create_books returns a list of 5 book poros with title, isbn, and publisher as attributes

---
+++ spec/facades/openlibrary_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe OpenlibraryFacade do
+  it 'returns a list of 5 book poros with title, isbn, and publisher as attributes', :vcr do
+    location = MapquestFacade.create_coordinates('denver,co')
+    forecast = OpenweatherFacade.create_forecast(location)
+    books = OpenlibraryFacade.create_books(location)
+
+    expect(books.count).to eq(5)
+    
+    books.each do |book|
+      expect(book).to be_a(Book)
+      expect(book.title).to be_a(String)
+      expect(book.isbn[0]).to be_a(String)
+      expect(book.publisher[0]).to be_a(String)
+    end 
+  end
+end
