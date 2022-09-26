e3762fa74e5239a13db8a0f0d3b62f88a39e14ab
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-25 21:25:29 -0600

    feat: ::get_forecast fetches weather data based on location

--- app/services/openweather_service.rb
+++ app/services/openweather_service.rb
@@ -3,4 +3,15 @@ class OpenweatherService
     Faraday.new(url: "https://api.openweathermap.org")
   end
 
+  def self.get_forecast(location)
+    response = conn.get('/data/2.5/onecall') do |req|
+      req.params['lat'] = location.lat
+      req.params['lon'] = location.lng
+      req.params['units'] = 'imperial'
+      req.params['exclude'] = 'minutely,alerts'
+      req.params['appid'] = ENV['weather_api_key']
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
