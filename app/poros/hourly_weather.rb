167186c371e9ac96044560680ceb5bb521ec6ab1
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-25 21:41:51 -0600

    feat: initialize HourlyWeather poro and attributes

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,13 @@
+class HourlyWeather
+  attr_reader :time,
+              :temperature,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @time = Time.at(data[:dt])
+    @temperature = data[:temp].to_f
+    @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:icon]
+  end
+end
