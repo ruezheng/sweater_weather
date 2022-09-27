e5427d79cc273401c9dd5184903f424640cc87d8
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 14:20:48 -0600

    feat:authenticate user params in #create action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user&.authenticate(params[:password])
-      render json: UserSerializer.new(user), status: 201
+      render json: UserSerializer.new(user), status: 200
     else
       render json: user.errors.full_messages.to_sentence, status: 400
     end
--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'User Log In' do
   describe '#create' do
     context 'happy path' do
-      it 'creates a new user' do
+      it 'creates a new user session and returns 200 status when credentials are valid' do
         user = User.create!(email: 'user@example.com', password: 'test123')
         api_key = user.generate_api_key
 
@@ -21,7 +21,7 @@ RSpec.describe 'User Log In' do
 
         body = JSON.parse(response.body, symbolize_names: true)
         
-        expect(response.status).to eq(201)
+        expect(response.status).to eq(200)
         expect(body).to have_key(:data)
         expect(body[:data].keys).to contain_exactly(:id, :type, :attributes)
         expect(body[:data][:attributes].keys).to contain_exactly(:email, :api_key)
