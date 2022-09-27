9e4e1d377856a8b9206b9149dc49b78196da6fba
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 14:00:21 -0600

    test: refactor sad path test to create a new user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < ApplicationController
+  include ParamsHelper
 
   def create
-    
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      render json: UserSerializer.new(user), status: 200
+    else
+      render json: user.errors.full_messages.to_sentence, status: 400
+    end
   end
 end
--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'User Registration' do
+RSpec.describe 'User Log In' do
   describe '#create' do
     context 'happy path' do
       it 'creates a new user' do
         params = {
           "email": 'user@example.com',
           "password": 'test123',
-          "password_confirmation": 'test123'
         }
         
         headers = {
@@ -65,8 +64,24 @@ RSpec.describe 'User Registration' do
       end
 
       it 'returns a 400 status error message if email is already taken' do
-        user = User.create!( email: 'user@example.com', password: 'test123', password_confirmation: 'test123' )
+        # create user1:
+        params = {
+          "email": 'user@example.com',
+          "password": 'test123',
+          "password_confirmation": 'test123'
+        }
         
+        headers = {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+
+        post '/api/v1/users', headers: headers, params: JSON.generate(params)
+        user1 = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(201)
+        
+        # user2: fails to create new user
         params = {
           "email": 'user@example.com',
           "password": 'test123',
