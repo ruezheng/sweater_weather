f6fd9c76d7cd6502dfe6e5cb6fc064a15c6e475d
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 11:12:16 -0600

    test: User model #generate_api_key creates a new api key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-brequire 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'relationships' do
@@ -10,6 +10,24 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:api_key) }
     it { should validate_uniqueness_of(:email) }
+  end
+
+  describe 'instance methods' do
+    describe '#generate_api_key' do
+      it 'creates a new api key' do
+        user = User.new({
+                          "email": 'user@example.com',
+                          "password": 'test123',
+                          "password_confirmation": 'test123'
+                        })
+
+        expect(user.api_key).to eq(nil)
+
+        user.generate_api_key
+
+        expect(user.api_key).to be_a(String)
+      end
+    end
   end
 end
  
