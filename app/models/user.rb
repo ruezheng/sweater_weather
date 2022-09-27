aa46859432e706355425beeedaa1456f62d5e95a
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 11:35:17 -0600

    fix: remove validation for api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
-  validates_presence_of :password_digest, :api_key
+  validates_presence_of :password_digest
 
   has_secure_password
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
-    it { should validate_presence_of(:api_key) }
     it { should validate_uniqueness_of(:email) }
   end
 
@@ -30,4 +29,3 @@ RSpec.describe User, type: :model do
     end
   end
 end
- 
