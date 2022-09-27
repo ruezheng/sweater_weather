720173a960b14a9e410e0659ce2fa49bc8eac5bc
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 16:03:08 -0600

    feat: create route 'POST /api/v1/road_trip'

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#index'
+      get '/book-search', to: 'books#search'
+      
+      resources :forecast, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
-
-      get '/book-search', to: 'books#search'
+      resources :road_trip, only: [:create]
     end
   end
 end
