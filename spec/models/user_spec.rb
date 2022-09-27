7c7ab41a0b6b2d6e385968e7fd51e3b43ad2da2f
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 10:48:14 -0600

    test: User model validates presence of api_key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:api_key) }
     it { should validate_uniqueness_of(:email) }
   end
 end
