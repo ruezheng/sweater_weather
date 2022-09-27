a516252bc132ded380dba4934de702f32e95ad1a
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 17:14:34 -0600

    feat: add ::format_city_and_state helper method to RoadTrip poro

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,21 @@
+class RoadTrip
+  attr_reader :id,
+              :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
+
+  def initialize(origin, destination, travel_time, weather_at_eta)
+    @id = nil
+    @start_city = format_city_and_state(origin)
+    @end_city = format_city_and_state(destination)
+    @travel_time = travel_time
+    @weather_at_eta = weather_at_eta
+  end
+
+  def self.format_city_and_state(location)
+    city = location.split(',').first.titleize
+    state = location.split(',').last.upcase
+    "#{city},#{state}"
+  end
+end
--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,14 +1,14 @@
 class RoadTripSerializer
 
-  def self.format_road_trip(route, forecast)
+  def self.format_road_trip(road_trip, forecast)
     {
       "data": {
         "id": null,
         "type": "roadtrip",
         "attributes": {
-          "start_city": "#{route.start_city.capitalize}, #{route.start_state.upcase}",
-          "end_city": "#{route.end_city.capitalize}, #{route.end_state.upcase}",
-          "travel_time": route.travel_time,
+          "start_city": "#{road_trip.start_city.capitalize}, #{road_trip.start_state.upcase}",
+          "end_city": "#{road_trip.end_city.capitalize}, #{road_trip.end_state.upcase}",
+          "travel_time": road_trip.travel_time,
           "weather_at_eta": {
             "temperature": forecast[:temperature],
             "conditions": forecast[:conditions]
