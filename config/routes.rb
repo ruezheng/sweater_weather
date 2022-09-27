530b6a73a6c78903a9ab7028f0ab3883a334c3c7
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 13:36:17 -0600

    feat: add route 'POST /api/v1/sessions #create'

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
 
       get '/book-search', to: 'books#search'
     end
   end
 end
-# GET /api/v1/book-search?location=denver,co&quantity=5
