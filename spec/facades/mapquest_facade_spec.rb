2e0a5ef9308c3fc6dddffab83b5a7bab711f9283
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 18:43:52 -0600

    test: MapquestFacade::create_route returns route data as a RoadTrip poro with travel_time and distance attributes

--- spec/facades/mapquest_facade_spec.rb
+++ spec/facades/mapquest_facade_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe MapquestFacade do
-  it 'returns a location poro with the city, state, lat, and lng', :vcr do
-    location = MapquestFacade.create_coordinates('denver,co')
+  context 'happy path', :vcr do
+    describe '::create_coordinates' do
+      it 'returns a location poro with the city, state, lat, and lng attributes' do
+        location = MapquestFacade.create_coordinates('denver,co')
 
-    expect(location).to be_a(Location)
-    expect(location.city).to be_a(String)
-    expect(location.state).to be_a(String)
-    expect(location.lat).to be_a(Float)
-    expect(location.lng).to be_a(Float)
+        expect(location).to be_a(Location)
+        expect(location.city).to be_a(String)
+        expect(location.state).to be_a(String)
+        expect(location.lat).to be_a(Float)
+        expect(location.lng).to be_a(Float)
+      end
+    end
+
+    describe '::create_route' do
+      it 'returns route data as a RoadTrip poro with travel_time and distance attributes' do
+        route = MapquestFacade.create_route('salt lake city,ut', 'new york,ny')
+
+        expect(route).to be_a(RoadTrip)
+        expect(route.travel_time).to be_a(String)
+        expect(route.distance).to be_a(String)
+      end    
+    end
   end
 end
