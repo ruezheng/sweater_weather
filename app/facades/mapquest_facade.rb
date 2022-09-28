7cfa98f68e1486c15f91758b4aeecf329a7870ca
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 18:44:59 -0600

    feat: add MapquestFacade::create_route

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -1,7 +1,13 @@
 class MapquestFacade
   def self.create_coordinates(location)
-    parsed_json = MapquestService.get_coordinates(location)
+    location_data = MapquestService.get_coordinates(location)
     
-    Location.new(parsed_json[0])
+    Location.new(location_data[0])
+  end
+  
+  def self.create_route(origin, destination)
+    route_data = MapquestService.get_route(origin, destination)
+
+    RoadTrip.new(origin, destination, route_data)
   end
 end
