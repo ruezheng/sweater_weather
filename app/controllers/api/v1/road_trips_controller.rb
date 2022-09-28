1c132f1d3a2afad2827bcb2d8615c08ff0bf4b82
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 18:28:50 -0600

    feat: add Mapquestservice::get_route

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::RoadTripsController < ApplicationController
 
   def create
-    
+    user = User.find_by(api_key: params[:api_key])
+
+    if user.present?
+      travel_time = MapquestFacade.get_travel_time(params[:origin], params[:destination])
+
+      weather_at_eta = OpenweatherFacade.create_weather_at_eta(params[:destination], travel_time)
+
+      road_trip = RoadTrip.new(params[:origin], params[:destination], travel_time, weather_at_eta)
+
+      render json: RoadTripSerializer.new(road_trip)
+    else
+      render json: { error: 'Api Key Incorrect or Missing' }, status: :unauthorized
+    end
   end
 end
