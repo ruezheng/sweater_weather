05a64174a5012f11e7059f669be949eade23d2bd
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-27 19:03:31 -0600

    test: add mapquest_route_response.json fixture to RoadTrip spec

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -10,7 +10,7 @@ class RoadTrip
     @id = nil
     @start_city = self.format_city_and_state(origin)
     @end_city = format_city_and_state(destination)
-    @travel_time = "#{route_data[:travel_time]} hrs"
+    @travel_time = format_time(route_data[:formattedTime])
     @distance = "#{route_data[:distance]} miles"
     # @weather_at_eta = weather_at_eta
   end
@@ -19,5 +19,12 @@ class RoadTrip
     city = location.split(',').first.titleize
     state = location.split(',').last.upcase
     "#{city},#{state}"
+  end
+
+  def format_time(route_data)
+    hours = route_data.split(':')[0]
+    minutes = route_data.split(':')[1]
+
+    "#{hours} hours, #{minutes} minutes"
   end
 end
--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -2,22 +2,25 @@ require 'rails_helper'
 
 RSpec.describe RoadTrip do
   it 'exists and has attributes' do
+    response = JSON.parse(File.read('spec/fixtures/mapquest_route_response.json'), symbolize_names: true)
+    route_data =  response[:route][:legs][0]
+
     weather_at_eta = {
       "temperature": 75.5,
       "conditions": 'not a cloud in the sky'
     }
 
-    road_trip = RoadTrip.new('salt lake city, ut', 'new york, ny', '33', weather_at_eta)
+    road_trip = RoadTrip.new('salt lake city, ut', 'new york, ny', route_data)
 
     expect(road_trip).to be_a(RoadTrip)
     expect(road_trip.id).to eq(nil)
     expect(road_trip.start_city).to eq('Salt Lake City, UT')
     expect(road_trip.end_city).to eq('New York, NY')
-    expect(road_trip.travel_time).to eq('33 hrs')
-    expect(road_trip.weather_at_eta).to eq({
-        "temperature": 75.5,
-        "conditions": 'not a cloud in the sky'
-      })
+    expect(road_trip.travel_time).to eq('31 hours, 18 minutes')
+    # expect(road_trip.weather_at_eta).to eq({
+      #   "temperature": 75.5,
+      #   "conditions": 'not a cloud in the sky'
+      # })
   end
 
   # xit 'returns even with impossible route' do
