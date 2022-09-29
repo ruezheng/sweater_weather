efb4ab26864170b07cd265d4673306cc940f59a9
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-29 08:44:37 -0600

    feat: pass test for #create

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::RoadTripController < ApplicationController
-  # include ParamsHelper
 
   def create
     user = User.find_by(api_key: params[:api_key])
 
     if user
-      route_data = MapquestFacade.create_route(params[:origin], params[:destination])
-
-      # weather_at_eta = OpenweatherFacade.create_weather_at_eta(params[:destination], travel_time)
-
-      trip_data = RoadTrip.new(params[:origin], params[:destination], route_data) # weather_at_eta
+      destination = MapquestFacade.create_coordinates(params[:road_trip][:destination])
       
-      render json: RoadTripSerializer.format_road_trip(trip_data)
+      travel_time = MapquestFacade.create_travel_time(params[:road_trip][:origin], params[:road_trip][:destination])
+      
+      weather_at_eta = OpenweatherFacade.create_weather_at_eta(destination, travel_time)
+
+      trip_data = RoadTrip.new(params[:road_trip][:origin], params[:road_trip][:destination], travel_time)
+      
+      render json: RoadTripSerializer.format_road_trip(trip_data, weather_at_eta)
     else
       render json: { error: 'Invalid API Key' }, status: :unauthorized
     end
