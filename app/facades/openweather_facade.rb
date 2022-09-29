0eee62da7ea85d25a4519bb51d0b89730c473109
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-29 07:55:06 -0600

    feat: add ::create_weather_at_eta

--- app/facades/openweather_facade.rb
+++ app/facades/openweather_facade.rb
@@ -7,10 +7,11 @@ class OpenweatherFacade
       WeatherForecast.new(parsed_json)
     end
 
-    def create_weather_at_eta(destination, hours)
-      parsed_json = OpenweatherService.get_forecast(destination)
-      
-      WeatherForecast.new(parsed_json)
+    def create_weather_at_eta(destination, travel_time)
+      forecast = OpenweatherService.get_forecast(destination)
+      travel_time_in_hours = travel_time.split(':').first.to_i
+
+      WeatherAtEta.new(forecast, travel_time_in_hours)
     end
   end
 end
